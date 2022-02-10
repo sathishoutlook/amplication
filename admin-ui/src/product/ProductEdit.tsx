e87f202188e80b9d9316d2b38834b616e282575e
sathishoutlook <99416692+sathishoutlook@users.noreply.github.com>
2022-02-10 21:31:20 +0530

    amplication-build-ckzh64c2829065001s6rg38r8pp

---
+++ admin-ui/src/product/ProductEdit.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import {
+  Edit,
+  SimpleForm,
+  EditProps,
+  TextInput,
+  NumberInput,
+} from "react-admin";
+
+export const ProductEdit = (props: EditProps): React.ReactElement => {
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <TextInput label="Description" multiline source="description" />
+        <NumberInput label="Item Price" source="itemPrice" />
+        <TextInput label="Name" source="name" />
+        <TextInput label="tgder" source="tgder" />
+      </SimpleForm>
+    </Edit>
+  );
+};
