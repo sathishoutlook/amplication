5a3ccab355a4cd5e14cea308b32689c36fe7f586
sathishoutlook <99416692+sathishoutlook@users.noreply.github.com>
2022-02-10 21:08:03 +0530

    amplication-build-ckzh5aes821963801s6nnj6wcbe

---
+++ admin-ui/src/product/ProductCreate.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import {
+  Create,
+  SimpleForm,
+  CreateProps,
+  TextInput,
+  NumberInput,
+} from "react-admin";
+
+export const ProductCreate = (props: CreateProps): React.ReactElement => {
+  return (
+    <Create {...props}>
+      <SimpleForm>
+        <TextInput label="Description" multiline source="description" />
+        <NumberInput label="Item Price" source="itemPrice" />
+        <TextInput label="Name" source="name" />
+        <TextInput label="tgder" source="tgder" />
+      </SimpleForm>
+    </Create>
+  );
+};
